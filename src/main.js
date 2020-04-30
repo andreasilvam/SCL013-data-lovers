af4b933986417fb0dc5cf98136641a97200f52b2
andreasilvam <62709543+andreasilvam@users.noreply.github.com>
2020-04-29 20:52:30 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
-import { example } from './data.js';
+/*import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(example, data);*/
+
+    import pokemon from './data/pokemon/pokemon.js';
+        
+     //console.log(pokemonData)
+
+     const data = pokemon.data;
+
+     const dataArreglo = Object.values(data);
+     console.log(pokemonData);
+
+     for(let i=0; i < dataArreglo.lenght; i++){
+         if (data[i].type.includes("poison") === true)
+     console.log(dataArreglo[i].name)
+}
+
+/*const root = document.getElementById("root");
+for (let i=0; i < dataArreglo.lenght; i++){
+root.innerHTML += "<p>Nombre: " + dataArreglo[i].name + "<p>"
+}*/
